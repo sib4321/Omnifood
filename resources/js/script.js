43142e3b9086a5bfedd3e3fa4fd58469135682f2
sib4321 <sib4321@ukr.net>
2017-09-05 13:05:44 +0300

    Add js files

---
+++ resources/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '25%'
+    });
+
+    /*
+    var waypoints = $('#handler-first').waypoint(function(direction) {
+     notify(this.element.id + ' hit 25% from top of window')
+     }, {
+     offset: '25%'
+     })
+     */
+});
